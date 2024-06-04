764dfd612913c086ca19b0565b5fe774fcde598a
kurosawa <kurosawa931@gmail.com>
2024-06-04 22:40:00 +0900

    textareaに自動改行を挿入するInputLineAddを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,57 @@
 "use client";
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useState } from "react";
 
 export default function Home() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [textAreaValue, setTextAreaValue] = useState<string>("");
 
+  // 30字毎にテキストに改行を挟む
+  const InputLineAdd = (text: string) => {// textは引数に渡したe.target.value
+    const inputLines = text.split('\n');//テキストの改行分割を変数化
+    const processedLines = inputLines.map((line: string) => {//Lineはmap配列の各要素
+      let result = "";//生成した文字列をここにadd joinされるまで配列化する
+      while (line.length > 30) {//Lineが30文字以上か確認
+        result += line.slice(0, 30) + '\n';//0~30文字をsliceし改行
+        line = line.slice(30);//30~最後尾までをslice、再度代入し変数を更新、再びループ
+      }
+      return result + line;//while終了後、余ったLine文字列をresultに追加
+    });
+    return processedLines.join('\n');//mapの戻り値の各配列を再統合する
+  };
+
+  // canvasに文字列を反映
   const handleInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const text = e.target.value;
+    const formattedText = InputLineAdd(e.target.value);
+    // 整形関数にvalueを渡す
+    setTextAreaValue(formattedText);
+    // 整形結果がstateに渡されtextarea更新
     const canvas = canvasRef.current;
-    // getContextではなくuseRefを使用した参照
-    // 初期値はnullに設定され、コンポーネントがマウントすると
-    // currentはcanvas要素を指す
     if (canvas) {
-    // canvasがnullでないことを確認
       const ctx = canvas.getContext('2d');
-      // ここで初めてgetContext 描画の準備が整う
-      console.log(ctx);
       if (ctx) {
-        // ctxがnullでないことを確認
-        ctx.clearRect(0, 0, canvas.width, canvas.height); //描画領域を初期化
-        ctx.font = "24px Arial";  // フォント設定を入力のたびに設定
-        ctx.fillStyle = "black";  // 色設定を入力のたびに設定
-        ctx.fillText(text, 10, 50); //e.target.valueを描画
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.font = "24px Arial";
+        ctx.fillStyle = "black";
+        ctx.fillText(formattedText, 10, 50);
       }
     }
   };
 
   return (
-    <div className="p-4">
+    <div className="p-4 bg-gray-200">
       <header className="mb-4">
       </header>
       <main>
         <canvas
           className="flex shadow-md rounded mx-auto border-gray-200 mb-4"
-          ref={canvasRef} width={500} height={250}  // サイズを大きくして確認
+          ref={canvasRef} width={500} height={250}
         />
         <textarea
+          className="shadow-md border border-gray-200"
+          rows={5}
+          value={textAreaValue}
           onChange={handleInput}
+          style={{ width: '100%', resize: 'none' }}
         />
       </main>
     </div>
