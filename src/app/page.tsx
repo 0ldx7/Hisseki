6ee512f74033c9ca369e580d8f7ea21f80450bef
kurosawa <kurosawa931@gmail.com>
2024-06-04 19:46:36 +0900

    ソースコードの解説を追加 textareaの横幅を微調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,20 @@ export default function Home() {
   const handleInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const text = e.target.value;
     const canvas = canvasRef.current;
+    // getContextではなくuseRefを使用した参照
+    // 初期値はnullに設定され、コンポーネントがマウントすると
+    // currentはcanvas要素を指す
     if (canvas) {
+    // canvasがnullでないことを確認
       const ctx = canvas.getContext('2d');
+      // ここで初めてgetContext 描画の準備が整う
+      console.log(ctx);
       if (ctx) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        // ctxがnullでないことを確認
+        ctx.clearRect(0, 0, canvas.width, canvas.height); //描画領域を初期化
         ctx.font = "24px Arial";  // フォント設定を入力のたびに設定
         ctx.fillStyle = "black";  // 色設定を入力のたびに設定
-        ctx.fillText(text, 10, 50);
+        ctx.fillText(text, 10, 50); //e.target.valueを描画
       }
     }
   };
@@ -25,10 +32,10 @@ export default function Home() {
       <main>
         <canvas
           className="flex shadow-md rounded mx-auto border-gray-200 mb-4"
-          ref={canvasRef} width={300} height={200}  // サイズを大きくして確認
+          ref={canvasRef} width={500} height={250}  // サイズを大きくして確認
         />
         <textarea
-          className="flex shadow-md border rounded mx-auto py-2 px-3 w-1/2 focus:outline-none leading-tight border-gray-200"
+          className="flex shadow-md rounded mx-auto py-2 px-3 w-2/3 max-w-lg  focus:outline-none leading-9 border-gray-200"
           placeholder="文字を入力" rows={5}
           onChange={handleInput}
         />
